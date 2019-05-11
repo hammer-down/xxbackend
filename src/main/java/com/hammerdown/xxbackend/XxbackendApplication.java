8c986d779e5c2d4c2239a360998ba135a91bc914
HulkLee1998 <33960917+HulkLee1998@users.noreply.github.com>
2019-05-11 13:57:17 +0800

    LH test 190511.

--- src/main/java/com/hammerdown/xxbackend/XxbackendApplication.java
+++ src/main/java/com/hammerdown/xxbackend/XxbackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class XxbackendApplication {
-
+    /**/
     public static void main(String[] args) {
         SpringApplication.run(XxbackendApplication.class, args);
     }
